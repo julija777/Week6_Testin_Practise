a9d9627075abcad4aeea43c4a433e3feea6623af
Julija777 <s.julija@icloud.com>
2021-12-20 15:41:50 +0000

    was trying to follow Chris in here

--- index.test.js
+++ index.test.js
@@ -1,8 +1,33 @@
 import { sum } from "./index.js";
 
+try {
 if (sum (2, 1) === 3) {
     console.log("All good");
 } else {
     console.log("Broken");
 }
 
+if (actual1 !== expected1) {
+    throw new Error(`${actual2} was not equal to ${expected1}`);
+
+}
+
+try {
+const actual2 = sum(150, 200);
+const expected2 = 350;
+
+if (actual2 !== expected2) {
+    throw new Error(`${actual2} was not equal to ${expected2}`);
+}
+}
+
+function test() {
+    try {
+        //run some code
+        console.log(`tick ${title}`);
+    } catch (err) {
+        console.log(`cross ${title}`);
+    }
+}
+
+test("1 + 2 should equal 3", function ())
