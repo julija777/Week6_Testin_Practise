1bbc267502599ba524b116977efc4f2c057cbccd
Julija Stepanova <32721917+julija777@users.noreply.github.com>
2021-12-20 14:52:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 export function sum(a, b) {
-  return a + b;
-}
+    return a + b;
+  }
+  if (sum(2, 1) === 3) {
+    console.log("All good");
+  } else {
+    console.log("Broken");
+  }
+  const total = sum();
